4789a917a1d65fe722cf5ad1439804fd075a6652
Grzegorz Wasilko <wasilko1@o2.pl>
2021-12-17 20:52:21 +0100

    new menu

---
+++ Doksy/Doksy/Menu.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Doksy
+{
+    class Menu
+    {
+        string[] options;
+        public Menu (string[] optionsMenu)
+        {
+            options = optionsMenu;
+        }
+
+
+        private int Longhest()
+        {
+            int length = 0;
+            for (int i =0;i<options.Length;i++)
+            {
+                if (options[i].Length > length)
+                {
+                    length = options[i].Length;
+                }
+            }
+            return length;
+        }
+        public int Open()
+        {
+            int length = Longhest();
+            int nr_option = 0;
+            ConsoleKeyInfo key;
+            do
+            {
+                Console.SetCursorPosition(0, 0);  //ustawiam kursor w prawym rogu ab menu rysowało sie w tym samym miejscu
+                for (int i = 0; i < options.Length; i++)
+                {
+                    if (i == nr_option)
+                    {
+                        Console.BackgroundColor = ConsoleColor.Blue;
+                    }
+                    else
+                    {
+                        Console.BackgroundColor = ConsoleColor.DarkBlue;
+                    }
+                    Console.WriteLine(options[i].PadRight(length));
+                }
+                key = Console.ReadKey(true);
+
+                if (key.Key == ConsoleKey.UpArrow && nr_option > 0)
+                {
+                    nr_option--;
+                }
+                else if (key.Key == ConsoleKey.DownArrow && nr_option < options.Length - 1)
+                {
+                    nr_option++;
+                }
+                else if (key.Key == ConsoleKey.Escape)
+                {
+                    nr_option = -1; // -1 option that does not exist.
+                } 
+            } while (key.Key != ConsoleKey.Escape && key.Key != ConsoleKey.Enter);
+
+            Console.ResetColor();
+            return nr_option;
+        }
+    }
+}
