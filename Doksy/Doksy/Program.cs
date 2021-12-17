a4a1d1da184bcc7869f319ab6ccdd368ae469dba
Grzegorz Wasilko <wasilko1@o2.pl>
2021-12-17 20:54:18 +0100

    program using menu

--- Doksy/Doksy/Program.cs
+++ Doksy/Doksy/Program.cs
@@ -10,6 +10,32 @@ namespace Doksy
     {
         static void Main(string[] args)
         {
+            Menu mainMenu = new Menu(new string[] {
+                "Customer Categorie",
+                "All avalible docks",
+                "Exit" });
+
+            int decision;
+            do
+            {
+                decision = mainMenu.Open();
+                if (decision == 0)
+                {
+                    Console.Clear();
+                    Console.WriteLine("Customer Categorie");
+                    Console.ReadKey();
+                }
+                else if (decision == 1)
+                {
+                    Console.Clear();
+                    Console.WriteLine("All avalible docks");
+                    Console.ReadKey();
+                }
+
+            } while (decision != 2 && decision != -1);
+
+
+
         }
     }
 }
